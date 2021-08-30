5e465ad8652b0daa3c2994ef8aea1827bfbe1734
faris ardi <faris.ardi@aladinbank.id>
2021-08-31 01:04:11 +0700

    fix page objects

--- features/step_definitions/android_step.js
+++ features/step_definitions/android_step.js
@@ -1,42 +1,23 @@
-'use strict'
 var { driver } = require('../../capabilities/android.caps') 
-
-
-// var profile = process.env.NODE_ENV
-// switch (profile) {
-//   case 'PLATFORM=ios':
-//     var { driver } = require('../../capabilities/ios.caps')
-//     var { Api_Page } = require('../ios/page_objects/api-demo_screen')
-//     break;
-//     case 'PLATFORM=android':
-      
-//       break;
-   
-//   default:
-//     console.log('Sorry, we are out of ' + profile + '.');
-// }
+var android = require('../android/page_objects/android_page')
 
 const assert = require('assert');
 const { Given, When, Then } = require('cucumber');
-// const Api = new Api_Page();
+const androidPO = new android();
+
 
 Given('open app', async () => {
-  return driver
-    .elementById("id.aladinbank.mobile.stg:id/btn_login")
-    .click()
-    .sleep(1000)
+  let ebtnLogin = await driver.elementById(androidPO.btnLogin);
+  await ebtnLogin.click().sleep(1000);
 });
 
 When('user login', async () => {
-  return driver
-    .elementById("id.aladinbank.mobile.stg:id/et_input_text")
-    .type("ayo@gmail.com")
+  let einputEmail = await driver.elementById(androidPO.inputEmail);
+  await einputEmail.type("ayo@gmail.com");
+ 
 });
 
 Then('user success login', async () => {
-  return driver
-    .elementById("id.aladinbank.mobile.stg:id/btn_login_email_next")
-    .click()
+  let ebtnNextLogin = await driver.elementById(androidPO.btnNextLogin);
+  await ebtnNextLogin.click();
 });
-
-
